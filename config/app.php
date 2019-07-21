867af09d515cd3d95f566e202d710a7292a5a0e1
Malik <abdlchoiri19@gmail.com>
2019-07-22 01:44:06 +0700

    require library maatwibsite/excel

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         App\Providers\ToolsServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -228,6 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'NaiveBayesClassifier' => App\Facades\NaiveBayesClassifier::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
