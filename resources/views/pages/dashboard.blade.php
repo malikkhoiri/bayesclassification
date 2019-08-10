4166a4018d0c3741ac53b72ce54a73062b3db6b0
Malik <abdlchoiri19@gmail.com>
2019-08-10 14:19:42 +0700

    refractor dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="main">
+        <div class="main-content">
+            <div class="container-fluid">
+                <div class="panel panel-headline">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Profile Perusahaan</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias deserunt dolorem doloremque eius error eveniet facilis harum in incidunt iusto labore, libero maxime necessitatibus nobis, obcaecati quo reiciendis? Suscipit, tenetur.</span><span>Aperiam asperiores consequuntur culpa delectus, deserunt fugiat laboriosam libero magni neque odit officiis provident quae quidem ratione sapiente sed sit, totam! Aut doloremque obcaecati officiis quasi quod, recusandae soluta veritatis?</span><span>A architecto aspernatur aut commodi, corporis delectus distinctio dolore ea ex exercitationem fugit itaque iure, iusto natus nemo odit quis quos rerum soluta sunt tempora temporibus ullam voluptatum. Deleniti, ipsa.</span><span>Aliquid architecto, culpa cum dolor dolore doloremque doloribus dolorum exercitationem ipsum iusto laudantium libero, minima molestiae nobis odit pariatur quam quo, rerum unde voluptates? Dolor minus non quam soluta veritatis!</span><span>Adipisci aperiam aspernatur beatae deleniti eligendi enim expedita explicabo facilis fugiat fugit harum id in iusto modi nam natus nisi nostrum odit placeat possimus quidem quo rem, reprehenderit tempora voluptates.</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
